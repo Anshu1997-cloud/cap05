58d7a99660c28cbd62c60f88c9bf124018c6a44f
Anshuman Chaturvedi <anshumanchaturvedi91@gmail.com>
2023-08-24 12:28:56 +0530

    adding prime no. code

---
+++ prime.js
@@ -0,0 +1,23 @@
+let num = 10;
+
+let count = 0;
+
+for(let i = 1; i<=num;i++)
+{
+
+if( num % i == 0)
+{
+
+cout++;
+
+}
+}
+
+if(count == 2)
+{
+console.log("Prime");
+}
+else
+{
+console.log("Not a Prime")
+}
